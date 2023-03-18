35cd5de8ef970135d527943a9b9b937e4d0bcb1b
Juanjo Asensio García <asengar2009@gmail.com>
2023-03-18 01:32:13 +0100

    Show the name in the menu at add_question

--- static/js/index.js
+++ static/js/index.js
@@ -82,46 +82,53 @@ function changePassword() {
 }
 
 function name(data) {
-  let divElement = document.getElementById("welcome")
   let divElement2 = document.getElementById("menu-name")
+  let divElement = document.getElementById("welcome")
   let div = document.getElementById("button-group");
   let modifyPassword = document.getElementById("modifyPasswordOption")
 
   if (data.User != undefined) {
     divElement2.innerHTML = data.User.name
-    div.innerHTML = `<button onclick="window.location.href='http://localhost:8080/add_question'" type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-plus-circle"></i> <br>Nueva Pregunta</button><button type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-pen"></i> <br>Modificar Pregunta</button><button type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-mortarboard"></i> <br>Preguntas de los Alumnos</button><button type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-bar-chart"></i> <br>Estadistica</button>`
-    if (data.User.password_changed == false) {
-      new bootstrap.Toast("#liveToast").show()
-    }
-    if (data.User.gender == "B") {
-      divElement.innerHTML = "Bienvenido " + data.User.name
-    }
-    else {
-      divElement.innerHTML = "Bienvenida " + data.User.name
+    if (window.location == "http://localhost:8080/") {
+      div.innerHTML = `<button onclick="window.location.href='http://localhost:8080/add_question'" type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-plus-circle"></i> <br>Nueva Pregunta</button><button type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-pen"></i> <br>Modificar Pregunta</button><button type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-mortarboard"></i> <br>Preguntas de los Alumnos</button><button type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-bar-chart"></i> <br>Estadistica</button>`
+      if (data.User.password_changed == false) {
+        new bootstrap.Toast("#liveToast").show()
+      }
+      if (data.User.gender == "B") {
+        divElement.innerHTML = "Bienvenido " + data.User.name
+      }
+      else {
+        divElement.innerHTML = "Bienvenida " + data.User.name
+      }
     }
   }
   else {
-    modifyPassword.style.cssText = "display: none !important;"
     divElement2.innerHTML = data.Guest.name
-    div.innerHTML = `<button onclick="window.location.href='http://localhost:8080/add_question'" type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-plus-circle"></i> <br>Nueva Pregunta</button>`
-    divElement.innerHTML = "Bienvenido/a " + data.Guest.name
+    if (window.location == "http://localhost:8080/") {
+      modifyPassword.style.cssText = "display: none !important;"
+      div.innerHTML = `<button onclick="window.location.href='http://localhost:8080/add_question'" type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-plus-circle"></i> <br>Nueva Pregunta</button>`
+      divElement.innerHTML = "Bienvenido/a " + data.Guest.name
+    }
   }
   resize()
 }
 function resize(role_guest) {
-  let divElement = document.getElementById("welcome");
-  let buttons = document.getElementsByClassName('button-group-element')
-  let buttons_array = Array.from(buttons)
-  let elemWidth = divElement.offsetWidth / buttons_array.length;
-  for (i = 0; i < buttons_array.length; i++) {
-    if (i != 0) {
-      buttons_array[i].style.marginLeft = elemWidth * i + "px"
+  if (window.location == "http://localhost:8080/") {
+    let divElement = document.getElementById("welcome");
+    let buttons = document.getElementsByClassName('button-group-element')
+    let buttons_array = Array.from(buttons)
+    let elemWidth = divElement.offsetWidth / buttons_array.length;
+    for (i = 0; i < buttons_array.length; i++) {
+      if (i != 0) {
+        buttons_array[i].style.marginLeft = elemWidth * i + "px"
+      }
+      buttons_array[i].style.width = elemWidth + "px"
     }
-    buttons_array[i].style.width = elemWidth + "px"
   }
 }
 
+let data = fetch("http://localhost:8080/api/auth").then((response) => response.json()).then((data) => (name(data)))
+
 if (window.location == "http://localhost:8080/") {
   window.addEventListener('resize', () => resize(), false)
-  let data = fetch("http://localhost:8080/api/auth").then((response) => response.json()).then((data) => (name(data)))
 }
