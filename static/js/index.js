536352c7ef2c48c634612b557ae5d610a4f47928
Juanjo Asensio García <asengar2009@gmail.com>
2023-04-11 21:23:01 +0200

    Alert when password changed

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,5 @@
+let index_page = false;
+
 function changePasswordVisibility(button, buttonNumber) {
   let field;
   if (buttonNumber == 0) {
@@ -24,7 +26,6 @@ function modifyPassword() {
     tooglePassword1: document.getElementById("toggle-password1").style,
     tooglePassword2: document.getElementById("toggle-password2").style,
     modal: bootstrap.Modal.getInstance("#changePasswordModal"),
-    successModal: new bootstrap.Modal("#changePasswordSuccessModal"),
     reset: function () {
       this.password.classList = "form-control"
       this.passwordAgain.classList = "form-control"
@@ -51,7 +52,7 @@ function modifyPassword() {
   }
   passwords.reset()
   if (passwords.verify()) {
-    fetch("http://localhost:8080/api/auth", {
+    fetch("/api/auth", {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json"
@@ -60,10 +61,10 @@ function modifyPassword() {
     }).then((response) => {
       if (response.ok) {
         passwords.modal.hide()
-        passwords.successModal.show()
+        window.location = "/?status=changePasswordSuccess"
       }
       else {
-        alert("Server Error!, try it again later")
+        window.location = "/?status=changePasswordError"
       }
     })
   }
@@ -89,7 +90,7 @@ function name(data) {
 
   if (data.User != undefined) {
     divElement2.innerHTML = data.User.name
-    if (window.location == "http://localhost:8080/") {
+    if (index_page) {
       div.innerHTML = `<button onclick="window.location.href='http://localhost:8080/add_question'" type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-plus-circle"></i> <br>Nueva Pregunta</button><button type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-pen"></i> <br>Modificar Pregunta</button><button type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-mortarboard"></i> <br>Preguntas de los Alumnos</button><button type="button" class="btn btn-outline-primary button-group-element"><i class="bi bi-bar-chart"></i> <br>Estadistica</button>`
       if (data.User.password_changed == false) {
         new bootstrap.Toast("#liveToast").show()
@@ -112,8 +113,15 @@ function name(data) {
   }
   resize()
 }
-function resize(role_guest) {
-  if (window.location == "http://localhost:8080/") {
+
+function resize_close() {
+  document.getElementById("alertSuccess").classList = "alert alert-success alert-dismissible fade d-none";
+  document.getElementById("alertError").classList = "alert alert-danger alert-dismissible fade d-none";
+  resize()
+}
+
+function resize() {
+  if (index_page) {
     let divElement = document.getElementById("welcome");
     let buttons = document.getElementsByClassName('button-group-element')
     let buttons_array = Array.from(buttons)
@@ -127,8 +135,29 @@ function resize(role_guest) {
   }
 }
 
-let data = fetch("http://localhost:8080/api/auth").then((response) => response.json()).then((data) => (name(data)))
+function prepare() {
+  let url = new URL(window.location.toLocaleString());
 
-if (window.location == "http://localhost:8080/") {
-  window.addEventListener('resize', () => resize(), false)
+  if (url.pathname = "/") {
+    index_page = true;
+    fetch("http://localhost:8080/api/auth").then((response) => response.json()).then((data) => (name(data)))
+    window.addEventListener('resize', () => resize(), false)
+  }
+
+  url = url.searchParams;
+
+  let alertSuccess = document.getElementById("alertSuccess");
+  let alertError = document.getElementById("alertError");
+  let messageSuccess = document.getElementById("alertSuccessMessage");
+  let messageError = document.getElementById("alertErrorMessage");
+
+  if (url.get("status") == "changePasswordSuccess") {
+    messageSuccess.innerHTML = "La contraseña se actualizó satisfactoriamente. Los cambios surgirán efecto la próxima vez que inicie sesión."
+    alertSuccess.classList = "alert alert-success alert-dismissible fade show";
+  }
+  if (url.get("status") == "changePasswordError") {
+    messageError.innerHTML = "Sucedió un error inesperado al actualizar la contraseña. Pruebe más adelante o contacte con el administrador."
+    alertError.classList = "alert alert-danger alert-dismissible fade show";
+  }
+
 }
